e3eb9afd50d35e1be2f1f568761d2e59e0d9fda9
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-12-29 12:48:26 +0545

    buildConnReq parseConnResp buildAnnounceReq

--- tracker.js
+++ tracker.js
@@ -3,6 +3,10 @@
 const dgram = require("dgram");
 const Buffer = require("buffer").Buffer;
 const urlParse = require("url").parse;
+const crypto = require("crypto");
+
+const torrentParser = require("./torrent-parser");
+const util = require("./util");
 
 module.exports.getPeers = (torrent, callback) => {
   const socket = dgram.createSocket("udp4");
@@ -17,7 +21,7 @@ module.exports.getPeers = (torrent, callback) => {
       const connResp = parseConnResp(res);
 
       //3. send announce request
-      const announceReq = buildAnnounceReq(connResp.connectionId);
+      const announceReq = buildAnnounceReq(connResp.connectionId, torrent);
       udpSend(socket, announceReq, url);
     } else if (respType(res) === "announce") {
       //4. parse announce res
@@ -36,12 +40,72 @@ module.exports.getPeers = (torrent, callback) => {
   function respType(resp) {}
 
   function buildConnReq() {
-    return "hello";
+    const buf = Buffer.alloc(16);
+
+    //connectionID
+    buf.writeUInt32BE(0x417, 0);
+    buf.writeUInt32BE(0x27101980, 4);
+
+    //action 0 --> connect
+    buf.writeUInt32BE(0, 8);
+
+    //transaction id
+    crypto.randomBytes(4).copy(buf, 12);
+    return buf;
   }
 
-  function parseConnResp(resp) {}
+  function parseConnResp(resp) {
+    return {
+      action: resp.readUInt32BE(0),
+      transactionId: resp.readUInt32BE(4),
+      connectionId: resp.slice(8),
+    };
+  }
 
-  function buildAnnounceReq(connId) {}
+  function buildAnnounceReq(connId, torrent, port = 6881) {
+    const buf = Buffer.allocUnsafe(98);
+
+    //connection id
+    connId.copy(buf, 0);
+
+    //action 1 --> announce
+    buf.writeUInt32BE(1, 8);
+
+    //transaction id
+    crypto.randomBytes(4).copy(buf, 12);
+
+    //info hash
+    torrentParser.infoHash(torrent).copy(buf, 16);
+
+    //peer id
+    util.genId().copy(buf, 36);
+
+    //downloaded
+    Buffer.alloc(8).copy(buf, 56);
+
+    //left
+    torrentParser.size(torrent).copy(buf, 64);
+
+    //uploaded
+    Buffer.alloc(8).copy(buf, 72);
+
+    //event  0-->none
+    buf.writeUInt32BE(0, 80);
+
+    //ip address
+    buf.writeUInt32BE(0, 84);
+
+    //key
+    crypto.randomBytes(4).copy(buf, 88);
+
+    //num want
+    buf.writeInt32BE(-1, 92);
+
+    //port
+    buf.writeUInt16BE(port, 96);
+
+    return buf;
+  }
 
   function parseAnnounceResp(resp) {}
 };
