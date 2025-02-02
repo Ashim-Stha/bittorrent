821ea595063b6737649228a30b4aef16a3c248d5
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-02-02 10:16:01 +0545

    onWholeMsg

--- src/download.js
+++ src/download.js
@@ -13,6 +13,29 @@ module.exports = (torrent) => {
 function download(peer) {
   const socket = net.Socket();
   socket.on("error", console.log);
-  socket.connect(peer.port, peer.ip, () => {});
-  socket.on("data", (data) => {});
+  socket.connect(peer.port, peer.ip, () => {
+    //socket.write(...) write a message here
+  });
+
+  onWholeMsg(socket, (data) => {
+    // This is the callback function
+    //handle response here
+  });
+  // socket.on("data", (data) => {});
+}
+
+function onWholeMsg(socket, callback) {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+
+  socket.on("data", (recvBuf) => {
+    const msglen = () =>
+      handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msglen()) {
+      callback(savedBuf.slice(0, msglen())); // Invoking the callback with the complete message
+      savedBuf = savedBuf.slice(msglen());
+      handshake = false;
+    }
+  });
 }
